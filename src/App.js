1608406dcc692ceee1f1a49d48ced0ad967191b2
HFG43lear <hernanguemes@hotmail.com>
2023-07-24 15:03:09 -0300

    chore: My Profile component in place with Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-
+import ProfilePage from './routes/ProfilePage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/Myprofile" element={<ProfilePage />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/components/MyProfile.js
@@ -0,0 +1,11 @@
+import styles from './MyProfile.module.css';
+
+function MyProfile() {
+  return (
+    <>
+      <div className={styles.my_profile}>My Profile</div>
+    </>
+  );
+}
+
+export default MyProfile;
