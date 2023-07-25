b507058476a3115daa239a952a425a3b4abbb978
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-25 15:36:25 -0600

    Add conditional components (buttons and badge)

--- src/components/RocketCard.js
+++ src/components/RocketCard.js
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { bookRocket, cancelRocket } from '../Redux/rockets/RocketsSlice';
+import styles from './Rockets.module.css';
 
 function RocketCard({ ArrRocket }) {
+  const dispatch = useDispatch();
   return (
     <li>
       <img src={ArrRocket.image} alt={ArrRocket.id} />
       <div>
         <h2>{ArrRocket.name}</h2>
-        <span>{ArrRocket.description}</span>
-        <button type="button">Reserve Rocket</button>
+        <span>
+          {
+            (ArrRocket.reserve) ? <span className={styles.reserve_span}>Reserved</span> : null
+          }
+          {ArrRocket.description}
+        </span>
+        {
+          (ArrRocket.reserve) ? <button className={styles.btn_cancel} type="button" onClick={() => dispatch(cancelRocket(ArrRocket.id))}>Cancel Reservation</button> : <button className={styles.btn_book} type="button" onClick={() => dispatch(bookRocket(ArrRocket.id))}>Reserve Rocket</button>
+        }
       </div>
     </li>
   );
@@ -19,6 +30,7 @@ RocketCard.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    reserve: PropTypes.bool,
   }).isRequired,
 };
 
