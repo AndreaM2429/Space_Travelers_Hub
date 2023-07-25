7f7207279c136e2e474e1b37be81a89f39e02ca7
HFG43lear <hernanguemes@hotmail.com>
2023-07-25 18:30:39 -0300

    feat: mission list render

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,15 +1,17 @@
-import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchMissions } from '../Redux/missions/MissionsSlice';
 
 function Missions() {
+  const { missions } = useSelector((state) => state.missions);
+
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchMissions());
+    if (missions.length === 0) {
+      dispatch(fetchMissions());
+    }
   }, [dispatch]);
 
-  const { missions } = useSelector((state) => state.missions);
   return (
     <>
       <tbody>
@@ -17,19 +19,17 @@ function Missions() {
           <tr key={mission.mission_id}>
             <td>{mission.mission_name}</td>
             <td>{mission.description}</td>
+            <td>
+              <button type="button">NOT A MEMBER</button>
+            </td>
+            <td>
+              <button type="button">Join Mission</button>
+            </td>
           </tr>
         ))}
       </tbody>
     </>
   );
 }
-
-Missions.propTypes = {
-  missions: PropTypes.shape({
-    mission_id: PropTypes.string.isRequired,
-    mission_name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-  }).isRequired,
-};
 
 export default Missions;
