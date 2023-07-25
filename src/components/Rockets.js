12810cc0d1894932a174aacf10304df7cd9f60a2
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-25 10:33:22 -0600

    Create the components to display the rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,17 @@
+import { useSelector } from 'react-redux';
+import RocketCard from './RocketCard';
+import styles from './Rockets.module.css';
+
 function Rockets() {
+  const { arrRock } = useSelector((state) => state.rockets);
   return (
-    <div>Rockets</div>
+    <section className={styles.rocket_container}>
+      <ul>
+        {arrRock.map((rocket) => (
+          <RocketCard key={rocket.id} ArrRocket={rocket} />
+        ))}
+      </ul>
+    </section>
   );
 }
 
