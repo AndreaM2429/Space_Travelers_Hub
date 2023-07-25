a5fb83bd568cd67c99e08088822b75252c5ac93d
Andre Manuel <andreamanuel2207@gmail.com>
2023-07-25 10:31:28 -0600

    Add the reducer for rockets

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './rockets/RocketsSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    rockets: rocketsReducer,
+  },
 });
 
 export default store;
