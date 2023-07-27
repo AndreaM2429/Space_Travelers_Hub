204d80f9a594eb9967a5a1488fe0078ed18e219a
HFG43lear <hernanguemes@hotmail.com>
2023-07-27 13:15:44 -0300

    feat: display joined mission in My Profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,12 +1,13 @@
 // import styles from './MyProfile.module.css';
+import JoinedMissionsList from './JoinedMissions';
 import RocketReserve from './RocketsReserve';
 import styles from './RocketsReserve.module.css';
 
 function MyProfile() {
   return (
     <section className={styles.profile_container}>
-      <div>
-        <span>Space for missions</span>
+      <div className={styles.title_reserve}>
+        <JoinedMissionsList />
       </div>
       <div className={styles.title_reserve}>
         <RocketReserve />
